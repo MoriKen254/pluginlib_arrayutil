6d8a668c5f9778f78074adee44b2b7af847d5aaf
MoriKen254 <masamorobo@gmail.com>
2017-06-03 15:07:24 +0900

    Add travis.

---
+++ include/pluginlib_arrayutil/ave.h
@@ -0,0 +1,11 @@
+#include <pluginlib_arrayutil/arrayutil_base.h>
+
+namespace arrayutil_plugins 
+{
+  class Ave : public arrayutil_base::ArrayUtil
+  {
+    public:
+      Ave();
+      double operate();
+  };
+}
